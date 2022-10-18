5d28c44931e87d01436897d3398195c13dcb5518
Shreyas Prabhu <csfxshreyas@gmail.com>
2022-10-18 22:04:37 +0530

    card details full width

--- src/components/Cards/CardDetails.jsx
+++ src/components/Cards/CardDetails.jsx
@@ -7,13 +7,13 @@ export const CardDetails = (props) => {
             <h6 className='card-subtitle mb-2 text-warning text-end'>{item.date}</h6>
             <h5 className='card-title'>{item.name.substring(0, 58)}</h5>
             <div>
-                <div className='row d-flex justify-content-start align-items-center w-75 mb-3'>
+                <div className='row d-flex justify-content-start align-items-center mb-3'>
                     <div className='col d-flex justify-content-between align-items-center'>
                         <span className='text-warning'>{item.size}</span>
                         <span className='text-primary'>{item.uploader}</span>
                     </div>
                 </div>
-                <div className='row d-flex justify-content-start align-items-center w-75 mb-3'>
+                <div className='row d-flex justify-content-start align-items-center mb-3'>
                     <div className='col d-flex justify-content-between align-items-center'>
                         <span className='text-success'>[S] {item.seeds}</span>
                         <span className='text-danger'>[L] {item.leeches}</span>
